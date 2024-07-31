70bcd0eace855dee502b82d1bd96860f0ab6f6b5
Leandro Galanterni <leandro@Leandros-MacBook-Pro-2.local>
2024-07-31 14:55:39 -0300

    add pg

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const express = require('express');
+import {config} from './dbconfig.js'
+import express from "express";
+
+import pkg from 'pg'
+const {Client} = pkg;
 
 const app = express()
 const PORT = 8000
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -9,6 +15,16 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.send('About route 🎉 ')
+})
+
+app.get('/canciones', async (req, res) => {
+  const client = new Client(config);
+  await client.connect();
+  let result = await client.query("select * from public.canciones");
+  await client.end();
+  console.log(result.rows);
+  res.send(result.rows)
+
 })
 
 app.listen(PORT, () => {
