54df04b75f7ecadc81a8a1d8aded2e9e38a4b5d2
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-08-23 22:53:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,36 +10,26 @@ const { Client } = pkg;
 app.use(express.json());
 app.use(cors());
 
-
-
 app.listen(PORT, () => {
   console.log(`API 👂 port ${PORT}`);
-})
+});
 
 app.get("/", (req, res) => {
   res.send(`API 🆗`);
-})
+});
 
 app.get("/canciones", async (req, res) => {
-  try{
+  try {
     console.log("Enviando todas las canciones...");
-    const qry = `SELECT public.canciones.id, public.canciones.nombre, 
-    public.artistas.nombre AS artista, albumes.nombre AS 
-    album, canciones.duracion, canciones.reproducciones
-  
-    FROM public.canciones
-  
-    JOIN public.albumes ON public.canciones.album = public.albumes.id
-    JOIN public.artistas ON public.albumes.artista = artistas.id
-    
-    WHERE public.canciones.id = ?;`;
-  
+    const qry = `SELECT * FROM 
+    public.canciones;`;
+
     const client = new Client(config);
     await client.connect();
     const result = await client.query(qry);
     await client.end();
-    res.send(result.rows);  
-  } catch(ex){
+    res.send(result.rows);
+  } catch (ex) {
     console.error(ex);
   }
-})
+});
