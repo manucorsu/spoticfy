7c7a549ab9218d85dec916ecaefd57ea56e27ba9
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-09-05 09:46:48 -0300

    login

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from "express";
 import { config } from "./dbconfig.js";
 import pkg from "pg";
 import cors from "cors";
+import bcrypt from "bcryptjs";
 
 const app = express();
 const PORT = 8000;
 const { Client } = pkg;
+const client = new Client(config);
 
 app.use(express.json());
 app.use(cors());
@@ -34,7 +36,6 @@ app.get("/canciones/:searchTxt", async (req, res) => {
     if (searchTxt != "*") {
       qry += ` WHERE public.canciones.nombre = '${searchTxt}';`;
     }
-    const client = new Client(config);
     await client.connect();
     const result = await client.query(qry);
     await client.end();
@@ -55,7 +56,6 @@ app.get("/albumes/:searchTxt", async (req, res) => {
     if (searchTxt != "*") {
       qry += ` WHERE public.albumes.nombre = '${searchTxt}';`;
     }
-    const client = new Client(config);
     await client.connect();
     const result = await client.query(qry);
     await client.end();
@@ -73,7 +73,6 @@ app.get("/artistas/:searchTxt", async (req, res) => {
     if (searchTxt != "*") {
       qry += ` WHERE public.artistas.nombre = '${searchTxt}';`; //esto está mal había que usar $ y recién me entero.
     }
-    const client = new Client(config);
     await client.connect();
     const result = await client.query(qry);
     await client.end();
@@ -83,18 +82,34 @@ app.get("/artistas/:searchTxt", async (req, res) => {
   }
 });
 
-app.post("/usuarios/", async (req, res) => {
-  try {
-    const client = new Client(config);
-    await client.connect();
-    let usuario = req.body;
-    let result = await client.query(
-      "INSERT INTO Usuarios (userid, email, nombre, password) VALUES ($1, $2, $3, $4)",
-      [usuario.userid, usuario.email, usuario.nombre, usuario.password]
-    );
-    await client.end();
-    res.send(result.rows);
-  } catch (ex) {
-    console.error(ex);
+app.post("/usuarios", async (req, res) => {
+  await client.connect();
+  const usuario = req.body;
+  const hashed = await bcrypt.hash(usuario.password, 10);
+  console.log("usuario", usuario);
+  console.log("hashed", hashed);
+  let result = await client.query(
+    "insert into usuarios(userid, email, nombre, password) values($1, $2, $3, $4)",
+    [usuario.userid, usuario.email, usuario.nombre, hashed]
+  );
+  await client.end();
+  console.log(result.rows);
+  res.send(result.rows);
+});
+
+app.post("/login/", async (req, res) => {
+  await client.connect();
+  const { userid, password } = req.body;
+  let result = await client.query("select * from usuarios where userid=$1", [
+    userid,
+  ]);
+  const usuario_db = result.rows[0];
+  const hashed = usuario_db.password;
+  const match = await bcrypt.compare(res.send(password, hashed));
+  if (match) {
+    res.send("OK");
+    return;
+  } else {
+    res.send("Los datos ingresados contienen un error.");
   }
 });
